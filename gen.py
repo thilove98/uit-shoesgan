da93b34972e872bdd8beba572665ab55c1ee5855
dqkqd <dqkqdlot@gmail.com>
2020-07-04 21:19:54 +0700

    add label model

--- gen.py
+++ gen.py
@@ -6,11 +6,19 @@ import numpy as np
 from torchvision import transforms, utils
 from model import Generator
 
+USE_LABEL = True
+
 SEED = 2020
 DEVICE = 'cuda'
+
 RESOLUTION = 512
-LATENT_SIZE = 128
 LEVELS = [4, 4, 8]
+
+LABELS_IN = True
+LABEL_SIZE = 11
+SHARED_DIM = 64
+
+LATENT_SIZE = 256 if USE_LABEL else 128
 
 def seed_everything(seed):
     random.seed(seed)
@@ -25,12 +33,19 @@ seed_everything(SEED)
 def load_model(model_path='model.pt'):
     # download model
     if not os.path.exists(model_path):
-        url_model = 'https://drive.google.com/uc?id=1gr6ghsrPX6CsEufFZkgMbDqLQ_KwsZaq'
+        if USE_LABEL:
+            url_model = 'https://drive.google.com/uc?id=1ChuVbLxYYbYdnqtid37LUEVkmHD5k18Z'
+        else:
+            url_model = 'https://drive.google.com/uc?id=1gr6ghsrPX6CsEufFZkgMbDqLQ_KwsZaq'
         gdown.download(url_model, output=model_path, quiet=False)
 
     # load checkpoint
-    checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
-    generator = Generator(RESOLUTION, LATENT_SIZE, 8, 2).to(DEVICE)
+    checkpoint = torch.load(model_path)
+
+    generator = Generator(RESOLUTION, LATENT_SIZE, 8, 2,
+        labels_in=True, label_size=LABEL_SIZE, shared_dim=SHARED_DIM,
+        ).to(DEVICE)
+    
     generator.load_state_dict(checkpoint['g_ema'])
     generator.eval()
     return generator
@@ -87,3 +102,34 @@ def get_random_images(n_samples, size, batch_size=16, model=MODEL):
             results.append(item)
             index += 1
     return results
+
+@torch.no_grad()
+def get_style_from_label(labels, model=MODEL):
+    """input:
+        - labels: a list of labels, i.e [1, 2, 3]
+        output:
+        - a numpy array of vector styles with shape: (len(labels), 320)
+    """
+    labels = torch.tensor(labels, dtype=torch.long, device=DEVICE)
+    latents = torch.randn(labels.shape[0], LATENT_SIZE, device=DEVICE)
+    styles = model.get_latent(latents, label=labels)
+    return styles.cpu().numpy()
+
+
+@torch.no_grad()
+def get_images_from_styles(style1, style2, style3, model=MODEL):
+    """input:
+        - style1, style2, style3 : vector with shape (320,)
+        output:
+        - image with size 512x512
+    """
+    styles = []
+    for i, style in enumerate([style1, style2, style3]):
+        style = torch.tensor(style, device=DEVICE)
+        style = style.repeat(LEVELS[i], 1)
+        styles.append(style)
+    styles = torch.cat(styles, 0).unsqueeze(0)
+    img, _ = model.([styles], input_is_latent=True)
+    img = img.cpu()
+    img = transforms.ToPILImage()(sample[0].clamp_(-1, 1).add_(1).div_(2 + 1e-5)).convert('RGB')
+    return img
