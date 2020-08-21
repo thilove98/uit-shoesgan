098db32be1f6b183478be903cde4972930239dde
dqkqd <dqkqdlot@gmail.com>
2020-08-22 05:51:27 +0700

    update

--- gen_test2.py
+++ gen_test2.py
@@ -1,42 +1,133 @@
+import os
+import random
+import json
 import numpy as np
 from PIL import Image
-import random
+
+class Generator:
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+SEED = 2020
+DEVICE = 'cuda'
+
+RESOLUTION = 512
+LEVELS = [4, 4, 8]
 
 LATENT_SIZE = 512
 
-def style_to_image(style1 = [], style2 = [], style3 = [], model=None, size = 512):
-    img = Image.new("RGB", (size, size), (random.randint(0, 254), random.randint(0, 254), random.randint(0, 254)))
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+
+seed_everything(SEED)
+
+def load_labels(labels_path='labels.json'):
+    with open(labels_path, 'r') as f:
+        labels = json.load(f)
+    return labels
+
+def load_predefined_styles(styles_path=os.path.join("styles-predefined", "styles-predefined.json")):
+    with open(styles_path, 'r') as f:
+        styles = json.load(f)
+    return styles
+
+MODEL = None
+LABELS = load_labels()
+STYLES = load_predefined_styles()
+
+def style_to_image(style1, style2, style3, model=MODEL):
+    return None
+    #return img
+
+def gen_images(batch, model=MODEL):
+    random_latents = torch.randn(batch, LATENT_SIZE).to(DEVICE)
+    samples, latents = model([random_latents], return_latents=True)
+    samples = samples.cpu()
+    latents = latents.cpu()
+    return samples, latents
+
+
+def get_random_images(inputs=[-1, -1, 2], model=MODEL):
+    results = []
+    for x in inputs:
+        if x == -1:
+            latent_z = torch.randn(1, LATENT_SIZE).to(DEVICE)
+            image, latent_w = model([latent_z], return_latents=True)
+            latent_w = latent_w[:, 0, :]
+        else:
+            latent_w = LATENTS[LABELS[x]].reshape(1, LATENT_SIZE)
+            latent_w = torch.from_numpy(latent_w).to(DEVICE)
+
+            image, test = model([latent_w], input_is_latent=True, return_latents=True)
+        
+        image = transforms.ToPILImage()(image[0].cpu().clamp_(-1, 1).add_(1).div_(2 + 1e-5))
+        latent_w = latent_w.cpu().numpy().reshape(-1,)
+        item = [image, latent_w]
+        results.append(item)
+    return results
+
+def get_style_from_label(labels):
+    name = labels[0]
+    results = LABELS[name]
+    labels = random.sample(results, len(labels))
+    labels = np.array(labels)
+
+    np.random.seed(labels)
+    latents = np.random.randint(0, 256, (3, 512))
+
+    return latents
+
+def get_style_from_random(n_samples, model=MODEL):
+    """input:
+        - n_samples: number of styles
+        output:
+        - a numpy array of vector styles with random 
+    """
+    return np.random.randint(0, 256, (n_samples, LATENT_SIZE))
+
+def get_style_from_index():
+    latents = []
+    styles = []
+    levels = []
+    for x in STYLES:
+        np.random.seed(x["id"])
+        latents.append(np.random.randint(0, 256, (512,)))
+        styles.append(x["style"])
+        levels.append(x["level"])
+    return latents, styles, levels
+
+def get_images_from_styles(vectors, weight=0.8, model=MODEL, multi_output=False):
+    vectors = np.sum(vectors, axis=1) / len(vectors[0])
+    vectors = np.clip(vectors, 0, 255).astype(np.uint8)
+    img = Image.new('RGB', (512, 512), color=tuple(map(int, np.random.randint(0, 256, (3,)))))
     return img
 
-def get_random_images(n, size):
-    ans = []
-    for i in range(n):
-        ans.append((style_to_image(size=70), [0.1, 0.2, 3], str(i)))
-    return ans
+def get_images_from_styles_mixing(input_style, mix_style, weight=0.8, model=MODEL, level=1):
+    style1 = np.array(input_style[0], dtype=np.float32)
+    style2 = np.array(input_style[1], dtype=np.float32)
+    style3 = np.array(input_style[2], dtype=np.float32)
 
-def get_random_image(size=512):
-    color = (
-        random.randint(0, 255),
-        random.randint(0, 255),
-        random.randint(0, 255)
-    )
-    latent = np.random.uniform(0, 1, size=(512,))
-    img = Image.new('RGB', (size, size), color=color)
-    return img, latent
+    styles = []
 
-def get_style_from_label(labels, model=None):
-    """input:
-        - labels: a list of labels, i.e [1, 2, 3]
-        output:
-        - a numpy array of vector styles with shape: (len(labels), 320)
-    """
-    return [[i, i, i, i*2, i*2] for i in range(len(labels))]
+    if level <= 3:
+        new_style = (input_style[level-1] + weight * mix_style[level-1]) / (weight + 1)
+        new_style = np.array(new_style, dtype=np.float32)
+        if level == 1:
+            style1 = new_style
+        elif level == 2:
+            style2 = new_style
+        elif level == 3:
+            style3 = new_style
+    else:
+        style1 = (input_style[0] + weight * mix_style[0]) / (weight + 1)
+        style2 = (input_style[1] + weight * mix_style[1]) / (weight + 1)
+        style1 = np.array(style1, dtype=np.float32)
+        style2 = np.array(style2, dtype=np.float32)
 
-def get_images_from_styles(style1, style2, style3, model=None):
-    """input:
-        - style1, style2, style3 : vector with shape (320,)
-        output:
-        - image with size 512x512
-    """
-    img = Image.new("RGB", (512, 512), (random.randint(0, 254), random.randint(0, 254), random.randint(0, 254)))
-    return img
+    img = Image.new('RGB', (512, 512), color=tuple(map(int, np.random.randint(0, 256, (3,)))))
+    return img, [style1, style2, style3]
+
--- test2.py
+++ test2.py
@@ -12,42 +12,62 @@ import struct
 import io
 import string
 
-app = Flask(__name__, template_folder=os.path.join("static", "test2"))
+app = Flask(__name__, template_folder=os.path.join('static', 'fancy3'))
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 def img2str(img):
     rawBytes = BytesIO()
     img.save(rawBytes, "PNG")
-    rawBytes.seek(0) 
+    rawBytes.seek(0)
     return base64.b64encode(rawBytes.read()).decode('utf-8')
 
 @app.route("/get_images_from_styles", methods=['POST', 'GET'])
 def stl2img():
     data = request.json
-    shape = np.array(data['Shape'])
-    detail = np.array(data['Detail'])
-    color = np.array(data['Color'])
-    img = get_images_from_styles(shape, detail, color)
+    vectors = data['vectors']
+    img = get_images_from_styles(vectors)
     return jsonify({"image": img2str(img)})
+
+@app.route("/get_images_from_styles_mixing", methods=['POST', 'GET'])
+def stlm2img():
+    data = request.json
+    input_style = np.array(data['input_style'])
+    mix_style = np.array(data['mix_style'])
+    weight = data['weight']
+    level = data['level']
+    img, vectors = get_images_from_styles_mixing(input_style, mix_style, weight=weight, level=level)
+
+    vectors = [[float(i) for i in vector] for vector in vectors]
+
+    return jsonify({"image": img2str(img), "vectors": vectors})
 
 @app.route("/get_style_from_label", methods=['POST', 'GET'])
 def lal2stys():
     data = request.json
     labels = np.array(data["labels"])
     vectors = get_style_from_label(labels)
-    vectors = [[i for i in vector] for vector in vectors]
+    vectors = [[float(i) for i in vector] for vector in vectors]
     return jsonify({"vectors": vectors})
 
 
-@app.route("/random_image", methods=['POST', 'GET'])
-def random_image():
-    image, latent = get_random_image()
-    latent = latent.tolist()
-    return jsonify({"image": img2str(image), "latent": latent})
+@app.route("/get_style_from_random", methods=['POST', 'GET'])
+def rnd2stl():
+    data = request.json
+    n_samples = np.array(data["num_images"])
+    vectors = get_style_from_random(n_samples)
+    vectors = [[float(i) for i in vector] for vector in vectors]
+    return jsonify({"vectors": vectors})
+
+@app.route("/load_predefined_styles", methods=['GET'])
+def load_predefined_styles():
+    latents, styles, levels = get_style_from_index()
+    latents = [[float(i) for i in latent] for latent in latents]
+    return jsonify({"vectors": latents, "styles": styles, "levels": levels})
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
-    return render_template("index.html")
+    return render_template("fancy.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
